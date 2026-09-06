929ddbe275874bda3a514c425707b25605d1b4b5
amViniciusD <Viniciusdiasaraujo12@gmail.com>
2026-09-06 19:28:24 -0300

    Add event listeners for three buttons

---
+++ script.js
@@ -0,0 +1,17 @@
+const botao1 = document.getElementById("botao1");
+
+botao1.addEventListener("click", function() {
+    alert("Você é pk xd")
+});
+
+const botao2 = document.getElementById("botao2");
+
+botao2.addEventListener("click", function() {
+    alert("O botao3 é fake news")
+});
+
+const botao3 = document.getElementById("botao3");
+
+botao3.addEventListener("click", function() {
+    alert("O City e o SPFC sao os melhores")
+});
